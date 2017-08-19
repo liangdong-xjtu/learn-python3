bd3d3a4f3e5c4b6e251e7d67a975a724b990cbb9
Dong Liang <liangdong_xjtu@163.com>
2017-08-19 21:19:54 +0800

    Add 10-hashlib.py

---
+++ 1-my-study/10-hashlib.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+md5 = hashlib.md5()
+md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
+print(md5.hexdigest())
+print(md5.digest())
+
+sha1 = hashlib.sha1()
+sha1.update('how to use sha1 in '.encode('utf-8'))
+sha1.update('python hashlib?'.encode('utf-8'))
+print(sha1.hexdigest())
+print(sha1.digest())
+print('\r\n')
+
+# name    | password
+# --------+----------
+# michael | 123456
+# bob     | abc999
+# alice   | alice2008
+# 
+# username | password
+# ---------+---------------------------------
+# michael  | e10adc3949ba59abbe56e057f20f883e
+# bob      | 878ef96e86145580c38c87f0410ad153
+# alice    | 99b1c2188db85afee403b1536010c2c9
+
+def calc_md5(password):
+	md5 = hashlib.md5();
+	md5.update(password.encode('utf-8'))
+	return md5.hexdigest()
+
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(username, password):
+	if username not in db:
+		print ('Username is not correct, please input the right one!')
+		return db.get(username, False)
+	passwd = calc_md5(password)
+	if db[username] == passwd:
+		return True
+	else:
+		return False
+
+def login_with_salt(username, password):
+	if username not in db:
+		print ('Username is not correct, please input the right one!')
+		return db.get(username, False)
+	passwd = calc_md5(password + username + 'the-Salt')
+	if db[username] == passwd:
+		return True
+	else:
+		return False
+
+def register(username, password):
+    db[username] = calc_md5(password + username + 'the-Salt')
+
+if __name__ == '__main__':
+# exercise 1
+	#password = input('Please input password:')
+	#print('The md5 hexdigest of your password is:')
+	#print(calc_md5(password))
+# exercise 2
+	username = input('Please input your username:')
+	password = input('Please input your password:')
+	print('Your password is:',login(username, password))
+# exercise 3
+	username = input('Register a new username now\r\nPlease input your username:')
+	password = input('Please input your password:')
+	register(username, password)
+
+	username = input('Login now\r\nPlease input your username:')
+	password = input('Please input your password:')
+	print('Your password is:',login_with_salt(username, password))
+
