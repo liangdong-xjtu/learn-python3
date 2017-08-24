a5f4b8b8a18035e23e600178c4c553ecdd9f6050
Dong Liang <liangdong_xjtu@163.com>
2017-08-24 22:26:22 +0800

    Refine oop_basic: samples/oop_basic/animals.py and samples/oop_basic/attrs.py

--- samples/oop_basic/animals.py
+++ samples/oop_basic/animals.py
@@ -30,3 +30,16 @@ print('d is Dog?', isinstance(d, Dog))
 print('d is Cat?', isinstance(d, Cat))
 
 run_twice(c)
+
+# 对于Python这样的动态语言来说，则不一定需要传入Animal类型。我们只需要保证传入的对象有一个run()方法就可以了
+# 这就是动态语言的“鸭子类型”，它并不要求严格的继承体系，一个对象只要“看起来像鸭子，走起路来像鸭子”，那它就可以被看做是鸭子。
+#  Python的“file-like object“就是一种鸭子类型。对真正的文件对象，它有一个read()方法，返回其内容。
+# 但是，许多对象，只要有read()方法，都被视为“file-like object“。许多函数接收的参数就是“file-like object“，
+# 你不一定要传入真正的文件对象，完全可以传入任何实现了read()方法的对象。
+
+class Timer(object):
+    def run(self):
+        print("Timer is running...")
+t = Timer()
+run_twice(t)
+#run_twice(Timer())
--- samples/oop_basic/attrs.py
+++ samples/oop_basic/attrs.py
@@ -23,3 +23,33 @@ print('getattr(obj, \'z\') =',getattr(obj, 'z', 404)) # 获取属性'z'，如果
 f = getattr(obj, 'power') # 获取属性'power'
 print(f)
 print(f())
+
+
+class Student(object):
+    name='Student'
+    def __init__(self,name):
+        self.name=name
+s=Student('Bob')
+print(s.name)#打印实例s的属性name
+print(Student.name)#打印类Student的属性name
+s.name='Mark'
+print(s.name)
+print(Student.name)
+del s.name
+print(s.name)
+print(Student.name)
+
+
+import json
+class Student(object):
+    name = "bob"
+    age = 15
+    sex = 1
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+s = Student("bob", 15 , 1)
+print(json.dumps(s, default=lambda obj: obj.__dict__))
+
+
