eba69086f6d55c9684a0c84aaae44cbc6f8e335e
Dong Liang <liangdong_xjtu@163.com>
2017-08-27 15:18:04 +0800

    Refine samples/functional/do_sorted.py

--- samples/functional/do_sorted.py
+++ samples/functional/do_sorted.py
@@ -11,5 +11,13 @@ print(sorted(L, key=str.lower))
 students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 print(sorted(students, key=itemgetter(0)))
+print(sorted(students, key=lambda x: x[0].lower()))
 print(sorted(students, key=lambda t: t[1]))
 print(sorted(students, key=itemgetter(1), reverse=True))
+# 甚至我们可以先对元组的第2个元素进行排序，然后对第一个元素进行排序，形成多级排序
+print(sorted(students, key=itemgetter(1,0), reverse=True))
+
+grade={'Lina':54,'Green':75,'Bob':79}
+print(sorted(grade,key=lambda x:x[0]))
+print(sorted(grade,key=lambda x:(str(x[0]).lower())))
+print(sorted(grade,key=lambda x:x[1])) # 答案无法解释 ???
